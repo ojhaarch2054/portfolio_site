1f0e3431bacb0d6aba9db0a38ac6898544297a11
Archana Ojha <t3ojar01@students.oamk.fi>
2025-03-31 16:09:28 +0300

    added simple layour of contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>
-      This is contact page
+    <div className='container'>
+      <h1 className="text-center">Contact Me</h1>
+      <div className="mb-4">
+        <label className="form-label">Email address:</label>
+        <input
+          type="email"
+          className="form-control"
+          id="input1"
+          placeholder="Enter your amail address"
+        />
+      </div>
+      <div className="mb-4">
+        <label className="form-label">Message:</label>
+        <textarea
+          className="form-control"
+          id="input2"
+          rows="5"
+        ></textarea>
+      </div>
+
+      <button>Send</button>
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
