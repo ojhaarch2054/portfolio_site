e4a78e4b8c98355ce5eff32ae9148e7127b2feea
Archana Ojha <t3ojar01@students.oamk.fi>
2025-03-31 09:32:23 +0300

    added text and image to homescreen

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from "react";
+import NavBar from "./NavBar";
+import picture from "../assets/archPicture.png";
+import "../styles/home.css";
 
 const HomePage = () => {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+      <div className="container mt-2">
+        <div className="img-container row align-items-center">
+          {/*for txt*/}
+          <div className="col-md-8">
+            <h3>
+              Hi, I'm Archana Ojha <br />
+              <small>Web Developer</small>
+              <br />
+              <small className="text-body-secondary">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque,
+                numquam placeat aliquam fuga impedit aut odio reprehenderit
+                perferendis minima natus quidem voluptas ut voluptatem repellat
+                nulla necessitatibus aspernatur, inventore eum.
+              </small>
+            </h3>
+          </div>
+
+          {/*for img*/}
+          <div className="col-md-4 text-center">
+            <img
+              src={picture}
+              alt="img"
+              className="img-fluid rounded-circle"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
