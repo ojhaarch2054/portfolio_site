acb4f7842165ce4c0881d5f9c43f27f65e5a24be
Archana Ojha <t3ojar01@students.oamk.fi>
2025-04-05 00:14:50 +0300

    added comment in a view website button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,13 +9,13 @@ import feedback from "../assets/feedback.png";
 import quote from "../assets/quote.jpg";
 import weather from "../assets/weather.png";
 
-
 const projects = [
   {
     id: 1,
     title: "Budget Tracker",
     image: budget,
-    description: "It is a web application which allows user to manage their daily finances. It is made using React, Bootstrap, Nodejs, express and PostgreSQL.This have has authentication and authorizattion feature. I have used JWT for authentication.",
+    description:
+      "It is a web application which allows user to manage their daily finances. It is made using React, Bootstrap, Nodejs, express and PostgreSQL.This have has authentication and authorizattion feature. I have used JWT for authentication.",
     link: "https://github.com/ojhaarch2054/Budget_tracker",
     githubLink: "https://ojhaarch2054/Budget_tracker",
   },
@@ -23,7 +23,8 @@ const projects = [
     id: 2,
     title: "Movie App",
     image: movie,
-    description: "This is a complete movie application made using React, Node.js, Express and PostgreSQL and was made in a group as a part of the coursework. The movie app enables users to explore movies, view showtimes, create groups and engage with other users.",
+    description:
+      "This is a complete movie application made using React, Node.js, Express and PostgreSQL and was made in a group as a part of the coursework. The movie app enables users to explore movies, view showtimes, create groups and engage with other users.",
     link: "https://github.com/Movie-App-Project-OAMK-G15/movie_app",
     githubLink: " https://github.com/Movie-App-Project-OAMK-G15/movie_app",
   },
@@ -31,7 +32,8 @@ const projects = [
     id: 3,
     title: "Quizzify",
     image: quiz,
-    description: "This app was developed in a group for our coursework. It is a quiz app in which user can take quizzes on various topics. The results from the quizzes are stored and the user can find insights on their performance on the quizzes. I worked in the UI design for this project using Bootstrap, HTML and CSS.",
+    description:
+      "This app was developed in a group for our coursework. It is a quiz app in which user can take quizzes on various topics. The results from the quizzes are stored and the user can find insights on their performance on the quizzes. I worked in the UI design for this project using Bootstrap, HTML and CSS.",
     link: "https://github.com/Gloryozo/Quizzify",
     githubLink: "https://github.com/Gloryozo/Quizzify",
   },
@@ -39,7 +41,8 @@ const projects = [
     id: 4,
     title: "Feedback Page",
     image: feedback,
-    description: "Created a feedback page using Bootstrap, Node.js, Javascript, Express, and PostgreSQL. The page stores customer details and feedback in a database. Also, the page user can give feedback by text and rating stars.",
+    description:
+      "Created a feedback page using Bootstrap, Node.js, Javascript, Express, and PostgreSQL. The page stores customer details and feedback in a database. Also, the page user can give feedback by text and rating stars.",
     link: "https://github.com/ojhaarch2054/Feedback_page",
     githubLink: "https://github.com/ojhaarch2054/Feedback_page",
   },
@@ -47,7 +50,8 @@ const projects = [
     id: 5,
     title: "Quote_App",
     image: quote,
-    description: "I made this website using HTML, CSS, JS, and API. You can share quotes on Twitter or Facebook by clicking the icons, or simply copy the quotes. Click the New Quote button to get a new quote whenever you want.",
+    description:
+      "I made this website using HTML, CSS, JS, and API. You can share quotes on Twitter or Facebook by clicking the icons, or simply copy the quotes. Click the New Quote button to get a new quote whenever you want.",
     link: "https://daily-quote-generater.onrender.com/",
     githubLink: "https://github.com/ojhaarch2054/Daily-Quote-generater",
   },
@@ -55,24 +59,27 @@ const projects = [
     id: 6,
     title: "Weather_App",
     image: weather,
-    description: "This is a simple website where you can check the current weather for different places. The website fetches weather data from the OpenWeatherMap API and displays it in a user-friendly format. I built it using React and OpenWeatherMap API to get the weather information online.",
+    description:
+      "This is a simple website where you can check the current weather for different places. The website fetches weather data from the OpenWeatherMap API and displays it in a user-friendly format. I built it using React and OpenWeatherMap API to get the weather information online.",
     link: "https://github.com/ojhaarch2054/Weather-info",
     githubLink: "https://github.com/ojhaarch2054/Weather-info",
   },
 ];
 
 const webClick = () => {
-  window.open(projects.link)
-}
+  window.open(projects.link);
+};
 
 const githubClick = () => {
-  window.open(projects.githubLink)
-}
+  window.open(projects.githubLink);
+};
 
 const Projects = () => {
   return (
     <div className="container mt-5 mx-3">
-      <h1 className="text-center mb-5 titleSize text-decoration-underline">My Projects</h1>
+      <h1 className="text-center mb-5 titleSize text-decoration-underline">
+        My Projects
+      </h1>
 
       <div className="d-flex align-items-center justify-content-center position-relative">
         {/*left btn to show previous project*/}
@@ -92,7 +99,10 @@ const Projects = () => {
               <div className="row justify-content-center ">
                 {/*it will map over the first 3 projects start from 0 index resulting in 3 item*/}
                 {projects.slice(0, 3).map((project) => (
-                  <div className="col-md-4 d-flex align-items-stretch mt-4 shadow " key={project.id}>
+                  <div
+                    className="col-md-4 d-flex align-items-stretch mt-4 shadow "
+                    key={project.id}
+                  >
                     <div className="card projectCard cardPart h-100  d-flex flex-column card shadow p-1">
                       <img
                         src={project.image}
@@ -102,12 +112,15 @@ const Projects = () => {
                       <div className="card-body infoPart flex-column text-center">
                         <h5 className="card-title">{project.title}</h5>
                         <p className="card-text">{project.description}</p>
-                        <a href={project.link} className="btn webBtn projectBtn">
+                        <a
+                          href={project.link}
+                          className="btn webBtn projectBtn"
+                          title="Haven't deployed yet"
+                        >
                           Visit Website
                         </a>
                         <a
-                          onClick={githubClick
-                          }
+                          onClick={githubClick}
                           className="btn webBtn mx-2 projectBtn"
                         >
                           <i className="bi bi-github"></i>
@@ -124,7 +137,10 @@ const Projects = () => {
               <div className="row justify-content-center">
                 {/*it will map over the projects from index 3 to 5 */}
                 {projects.slice(3, 6).map((project) => (
-                  <div className="col-md-4 d-flex align-items-stretch shadow" key={project.id}>
+                  <div
+                    className="col-md-4 d-flex align-items-stretch shadow"
+                    key={project.id}
+                  >
                     <div className="card cardPart d-flex flex-column card shadow p-1">
                       <img
                         src={project.image}
@@ -134,12 +150,14 @@ const Projects = () => {
                       <div className="card-body infoPart flex-column  text-center ">
                         <h5 className="card-title">{project.title}</h5>
                         <p className="card-text">{project.description}</p>
-                        <a href={project.link} className="btn webBtn projectBtn">
+                        <a
+                          href={project.link}
+                          className="btn webBtn projectBtn"
+                        >
                           Visit Website
                         </a>
                         <a
-                          onClick={webClick
-                          }
+                          onClick={webClick}
                           className="btn webBtn mx-2 projectBtn"
                         >
                           <i className="bi bi-github"></i>
